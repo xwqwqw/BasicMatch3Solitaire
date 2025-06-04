f04e45313b4bf4a30594a4042402b9e4ab16ca77
Kaan <taha-kaan-2002@outlook.com>
2025-06-04 21:31:50 +0300

    Fixed win logic

--- Assets/_Main/Scripts/CardSystem/CardStack.cs
+++ Assets/_Main/Scripts/CardSystem/CardStack.cs
@@ -1,73 +1,98 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using _Main.Scripts.CardSystem;
 using _Main.Scripts.Controllers;
 using UnityEngine;
 
-namespace _Main.Scripts.CardSystem
+public class CardStack : MonoBehaviour
 {
-    public class CardStack : MonoBehaviour
+    public bool allowMatch = true;
+
+    private readonly List<CardView> _cards = new();
+
+    private static readonly List<CardStack> AllStacks = new();
+
+    private void OnEnable()
     {
-        public bool allowMatch = true; 
+        AllStacks.Add(this);
+    }
 
-        private readonly List<CardView> _cards = new();
-   
-        public CardView TopCard => _cards.Count > 0 ? _cards[^1] : null;
+    private void OnDisable()
+    {
+        AllStacks.Remove(this);
+    }
 
-        public bool CanTake(CardView card)
+    public CardView TopCard => _cards.Count > 0 ? _cards[^1] : null;
+
+    public bool CanTake(CardView card)
+    {
+        return _cards.Count > 0 && _cards[^1] == card;
+    }
+
+    public void AddCard(CardView card)
+    {
+        _cards.Add(card);
+        card.transform.SetParent(transform);
+        card.transform.localPosition = new Vector3(0, -20 * (_cards.Count - 1), 0);
+        card.CurrentStack = this;
+
+        if (allowMatch)
         {
-            return _cards.Count > 0 && _cards[^1] == card;
+            CheckMatch();
+        }
+    }
+
+    public void RemoveCard(CardView card)
+    {
+        if (CanTake(card))
+        {
+            _cards.Remove(card);
         }
 
-        public void AddCard(CardView card)
-        {
-            _cards.Add(card);
-            card.transform.SetParent(transform);
-            card.transform.localPosition = new Vector3(0, -20 * (_cards.Count - 1), 0);
-            card.CurrentStack = this;
+        StartCoroutine(CheckStacksAfterDelay());
+    }
 
-            if (allowMatch)
-            {
-                CheckMatch();
-            }
+    private IEnumerator CheckStacksAfterDelay()
+    {
+        yield return new WaitForSeconds(0.15f);
+        if (AreAllStacksEmpty())
+        {
+            CheckLevelWin();
+        }
+    }
+
+    private static bool AreAllStacksEmpty()
+    {
+        return AllStacks.All(stack => stack._cards.Count == 0);
+    }
+
+    private void CheckLevelWin()
+    {
+        EventController.Trigger(EventNames.OnLevelWin, true);
+    }
+
+    private void CheckMatch()
+    {
+        if (_cards.Count < 3) return;
+
+        var lastThree = _cards.GetRange(_cards.Count - 3, 3);
+        if (lastThree.Any(c => c.CardId != lastThree[0].CardId)) return;
+
+        foreach (var cardView in lastThree)
+        {
+            _cards.Remove(cardView);
+            Destroy(cardView.gameObject);
         }
 
-        public void RemoveCard(CardView card)
-        {
-            if (CanTake(card))
-            {
-                _cards.Remove(card);
-            }
-        
-            if (!allowMatch && _cards.Count <= 0) CheckLevelWin();
-        }
+        UpdateCardPositions();
+    }
 
-        private void CheckLevelWin()
+    private void UpdateCardPositions()
+    {
+        for (int i = 0; i < _cards.Count; i++)
         {
-            EventController.Trigger(EventNames.OnLevelWin, true);
-        }
-
-        private void CheckMatch()
-        {
-            if (_cards.Count < 3) return;
-
-            var lastThree = _cards.GetRange(_cards.Count - 3, 3);
-            if (lastThree.Any(c => c.CardId != lastThree[0].CardId)) return;
-        
-            foreach (var cardView in lastThree)
-            {
-                _cards.Remove(cardView);
-                Destroy(cardView.gameObject);
-            }
-        
-            UpdateCardPositions();
-        }
-
-        private void UpdateCardPositions()
-        {
-            for (int i = 0; i < _cards.Count; i++)
-            {
-                _cards[i].transform.localPosition = new Vector3(0, -30 * i, 0);
-            }
+            _cards[i].transform.localPosition = new Vector3(0, -30 * i, 0);
         }
     }
 }
